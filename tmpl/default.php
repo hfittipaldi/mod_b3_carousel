1971c1bcb7ac72808e4299ee8d40a39c93c8a772
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2016-09-03 15:11:16 -0300

    Fix wrong links at the blob controls

--- tmpl/default.php
+++ tmpl/default.php
@@ -62,11 +62,11 @@ if ($images !== null) :
 
     <?php if ($controls === 1) : ?>
     <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-<?php echo $module_id; ?>" role="button" data-slide="prev">
+    <a class="left carousel-control" href="#b3Carousel-<?php echo $module_id; ?>" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#carousel-<?php echo $module_id; ?>" role="button" data-slide="next">
+    <a class="right carousel-control" href="#b3Carousel-<?php echo $module_id; ?>" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
